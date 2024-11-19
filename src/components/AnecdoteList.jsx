4b6b3ce87bcaaf2c882c69f5d18a36daee2217fe
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-11-19 18:30:30 +0100

    exercise 6.20

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -1,33 +1,42 @@
 import { useSelector, useDispatch } from "react-redux";
 import { castVote } from "../reducers/anecdoteReducer";
 import { setNotification } from "../reducers/notificationReducer";
+import { useQuery } from "@tanstack/react-query";
+import anecdotesService from "../services/anecdotes";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(({ filter, anecdotes }) => {
-    if (filter === "") {
-      return anecdotes;
-    }
-    return filter !== ""
-      ? anecdotes.filter((a) => a.content.includes(filter))
-      : anecdotes;
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: anecdotesService.getAll,
+    retry: 1,
+  });
+
+  const anecdotes = result.data;
+
+  const filterState = useSelector(({ filter }) => {
+    return filter;
   });
 
   const dispatch = useDispatch();
 
   const vote = (id) => {
     dispatch(castVote(id));
-    const votedAnecdote = anecdotes.find((a) => a.id === id);
+    const votedAnecdote = result.data?.find((a) => a.id === id);
 
     dispatch(setNotification(`You voted ${votedAnecdote.content}`, 5000));
   };
 
-  // if (result.isError) {
-  //   return <span>Error: {error.message}</span>
-  // }
+  if (result.isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (result.isError) {
+    return <span>Anecdote service not available due to error</span>;
+  }
 
   return (
     <div>
-      {anecdotes.map((anecdote) => (
+      {anecdotes?.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
